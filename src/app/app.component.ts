3baec78dd22a888900550b8e81037155a1ba241b
Wan Muzaffar <wanmuzaffar@Wans-MacBook-Pro.local>
2021-01-25 12:43:58 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+	// Create 3 variables
+	// Name - string, age - number, location (no type)
+  name : String = "Muzaffar"
+  age : Number = 30
+  location = "Equine Park"
+  num1 : Number = 10;
+  num2 = 3
+  // third data type
+  lapar = true
+  penat = false
+  // Array
+  scores = [30,25,10,20,60];
+  // Object
+  biodata = {"name":"Muzaffar","umur":30, 
+  "location":"Equine Park"}
+
+
+  // TO bring this out in HTML we use interpolation {{}}
 }
